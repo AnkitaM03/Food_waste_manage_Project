0fdd5653a52ad5a214aa02ae5440a12e9f8eae3f
Darshan Narsingkar <narsingdarsh8802@gmail.com>
2025-01-28 14:58:08 +0530

    last commit

--- food-waste-management-system-main/connection.php
+++
@@ -1,7 +0,0 @@
-
-
-<?php
-//change mysqli_connect(host_name,username, password); 
-$connection = mysqli_connect("localhost", "root", 'Ankita@2003');
-$db = mysqli_select_db($connection, 'demo');
-?>
